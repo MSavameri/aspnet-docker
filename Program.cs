8f33af1514764403c617487ab2c2401ecbc6f86d
Meysam <meysam.savameri@gmail.com>
2025-04-18 21:58:51 +0330

    Add test comment

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//add for test
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
